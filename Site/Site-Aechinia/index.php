0e250939c0f9790bf56345112abfadf3dd8024bc
MatheusEugenioMoreira1 <59825515+MatheusEugenioMoreira1@users.noreply.github.com>
2020-07-19 10:07:48 -0300

    Add files via upload

---
+++ Site/Site-Aechinia/index.php
@@ -0,0 +1,48 @@
+<html>
+<head>
+
+
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/styles.css">
+
+<title>Aechinia</title>
+</head>
+
+<body>
+
+<div class="div-header">
+    <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-10">
+            <img src="img/Logo_Oficial.svg" height="87px" width="88px" alt="Logo Aechinia" style="margin-bottom: 60px; margin-top: 40px">
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-4">
+            <p class="text-header">Tenha um amparo amigo sem sair de casa.</p>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-4">
+            <button class="saiba-mais">Saiba mais!</button>
+        </div><!--OIIIIIIIIIIIII SEUUUUUUUUUUUUU PUTTTTTTTTTTTTOOOOOOOOOOOOOOOOOOOO-->
+    </div>
+</div>
+<div class="div-quem_somos">
+    <div class="row">
+        <div class="col-md-3"></div>
+        <div class="col-md-6" style="text-align: center">
+            <p class="title-text-quem">Quem somos</p>
+            <p class="content-text-quem">aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaQuem somos</p>
+        </div>
+        <div class="col-md-3"></div>
+    </div>
+</div>
+
+</body>
+</html>
